24688481469aa5296cb727f1c9b9c0cba7a65c8e
Paul Cadman <git@paulcadman.dev>
2022-06-21 13:31:55 +0100

    Add golden tests for milestone examples

--- test/BackendC.hs
+++ test/BackendC.hs
@@ -1,7 +1,8 @@
 module BackendC where
 
 import BackendC.Positive qualified as P
+import BackendC.Examples qualified as E
 import Base
 
 allTests :: TestTree
-allTests = testGroup "Backend C tests" [P.allTests]
+allTests = testGroup "Backend C tests" [P.allTests, E.allTests]
